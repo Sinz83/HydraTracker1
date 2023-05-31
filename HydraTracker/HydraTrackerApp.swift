5bec77b7cca38c9879e4486da58fce16d641bf50
Sabrina Inzillo <petitcomiteparis@gmail.com>
2023-05-31 11:28:05 +0200

    Initial Commit

---
+++ HydraTracker/HydraTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydraTrackerApp.swift
+//  HydraTracker
+//
+//  Created by Sabrina Inzillo on 31.05.23.
+//
+
+import SwiftUI
+
+@main
+struct HydraTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
